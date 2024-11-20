ffa36163b051f40034fd9d23d0a25a841693a0c2
Pavel <pashapasha-2013@mail.ru>
2024-11-20 18:23:58 +0300

    solution1

---
+++ solution1/solution.py
@@ -0,0 +1,16 @@
+def strict(func):
+    def wrapper(*args, **kwargs):
+        types = func.__annotations__
+        if type(args[0]) is types['a'] and type(args[1]) is types['b']:
+            return func(*args, **kwargs)
+        else:
+            raise TypeError
+    return wrapper
+
+@strict
+def sum_two(a: int, b: int) -> int:
+    return a + b
+
+
+print(sum_two(1, 2))  # >>> 3
+print(sum_two(1, 2.4))  # >>> TypeError
